26d6e91e8bde2b8e8e3aefb39726b4b92dee32db
AlexHtZhang <alexander.ht.zhang@gmail.com>
2018-09-04 12:35:57 -0700

    new user add funciton

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+// io.on("connection", socket => {}) here the socket is indivadual socket for each user.
+// one side emit another side use socket.on to recieve the emit from another side.
 const path = require("path");
 const http = require("http");
 const express = require("express");
@@ -15,13 +17,29 @@ io.on("connection", socket => {
   console.log("New user connected");
 
   socket.emit("newMessage", {
-    from: "name",
-    text: "content",
-    createdAt: 123123
+    from: "Admin",
+    text: "Welcome to the anochat",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
   });
 
   socket.on("createMessage", message => {
     console.log("createMessage", message);
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", () => {
